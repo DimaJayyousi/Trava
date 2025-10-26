e7f2305abbf9b996a9180f043db54c6a3e739a53
Dima Jayyousi <dimajayousi@gmail.com>
2025-10-26 21:03:35 +0300

    home page implementation done

---
+++ lib/pages/home.dart
@@ -0,0 +1,249 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Image.asset(
+                  "images/header.jpeg",
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.5,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, right: 20.0),
+                  child: Row(
+                    children: [
+                      Material(
+                        elevation: 3.0,
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Image.asset(
+                            "images/pin.png",
+                            height: 30,  // Reduced size
+                            width: 30,   // Reduced size
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Material(
+                        elevation: 3.0,
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.deepPurple,
+                            size: 30.0,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10.0),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(60),
+                        child: Image.asset(
+                          "images/profile.jpeg",
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 180.0, left: 18.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Trava",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 70.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Travel Community App",
+                        style: TextStyle(
+                          color: const Color(0xDAFFFFFF),
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 30.0,
+                    right: 30.0,
+                    top: MediaQuery.of(context).size.height / 2.7,
+                  ),
+                  child: Material(
+                    elevation: 7.0,
+                    borderRadius: BorderRadius.circular(15.0), // Added border radius to Material
+                    child: Container(
+                      padding: EdgeInsets.only(left: 20.0),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(108, 255, 255, 255),
+                        border: Border.all(width: 1.5),
+                        borderRadius: BorderRadius.circular(15.0), // Increased border radius
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search your destination",
+                          suffixIcon: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40.0),
+            Container(
+              margin: EdgeInsets.only(left: 10.0, right: 10.0),
+              child: Material(
+                elevation: 3.0,
+                borderRadius: BorderRadius.circular(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 15.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0, left: 10.0),
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(30),
+                              child: Image.asset(
+                                "images/profile.jpeg",
+                                height: 50,
+                                width: 50,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(width: 15.0),
+                            Text(
+                              "Dima Jayyousi",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset("images/Place_one.jpeg"),
+                      ),
+                      SizedBox(height: 5.0),
+                      Row(
+                        children: [
+                          Icon(Icons.location_on, color: Colors.deepPurple),
+                          SizedBox(width: 5.0),
+                          Text(
+                            "Japan",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          "this place is really beautiful and peaceful",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.favorite_outlined,
+                            color: Colors.black54,
+                            size: 30.0,
+                          ),
+                          SizedBox(width: 10.0),
+                          Text(
+                            "Like",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          SizedBox(width: 20.0),
+                          Icon(
+                            Icons.comment_outlined,
+                            color: Colors.black54,
+                            size: 30.0,
+                          ),
+                          SizedBox(width: 10.0),
+                          Text(
+                            "Comment",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 30.0),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 41:39
